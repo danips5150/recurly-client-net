a06b34918522dbfc5a22b2076136e2234eef24bb
dpena <dpena@VE-BI-PC-031>
2017-12-11 17:31:47 +0100

    fix unit amounts exceptions when using percentage addons

--- Library/SubscriptionAddOn.cs
+++ Library/SubscriptionAddOn.cs
@@ -1,10 +1,12 @@
-﻿using System.Xml;
+﻿using System;
+using System.Xml;
 
 namespace Recurly
 {
     public class SubscriptionAddOn : RecurlyEntity
     {
         public string AddOnCode { get; set; }
+        public AddOn.Type AddOnType { get; set; }
         public int UnitAmountInCents { get; set; }
         public int Quantity { get; set; }
         public Adjustment.RevenueSchedule? RevenueScheduleType { get; set; }
@@ -14,9 +16,10 @@ namespace Recurly
             ReadXml(reader);
         }
 
-        public SubscriptionAddOn(string addOnCode, int unitAmountInCents, int quantity = 1)
+        public SubscriptionAddOn(string addOnCode, AddOn.Type addOnType, int unitAmountInCents, int quantity = 1)
         {
             AddOnCode = addOnCode;
+            AddOnType = addOnType;
             UnitAmountInCents = unitAmountInCents;
             Quantity = quantity;
         }
@@ -36,12 +39,18 @@ namespace Recurly
                         AddOnCode = reader.ReadElementContentAsString();
                         break;
 
+                    case "add_on_type":
+                        AddOnType = reader.ReadElementContentAsString().ParseAsEnum<AddOn.Type>();
+                        break;
+
                     case "quantity":
                         Quantity = reader.ReadElementContentAsInt();
                         break;
 
                     case "unit_amount_in_cents":
-                        UnitAmountInCents = reader.ReadElementContentAsInt();
+                        int unitAmountInCents;
+                        if (Int32.TryParse(reader.ReadElementContentAsString(), out unitAmountInCents))
+                            UnitAmountInCents = unitAmountInCents;
                         break;
 
                     case "revenue_schedule_type":
@@ -57,7 +66,8 @@ namespace Recurly
 
             writer.WriteElementString("add_on_code", AddOnCode);
             writer.WriteElementString("quantity", Quantity.AsString());
-            writer.WriteElementString("unit_amount_in_cents", UnitAmountInCents.AsString());
+            if (AddOnType == AddOn.Type.Fixed)			
+                writer.WriteElementString("unit_amount_in_cents", UnitAmountInCents.AsString());
 
             if (RevenueScheduleType.HasValue)
                 writer.WriteElementString("revenue_schedule_type", RevenueScheduleType.Value.ToString().EnumNameToTransportCase());
